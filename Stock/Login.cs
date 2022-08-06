dd6818bf3b5caaf1749be77fcd3c5a8f6d25b413
hubbychops <47495913+hubbychops@users.noreply.github.com>
2022-08-06 11:28:47 +0100

    stage 2

---
+++ Stock/Login.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stock
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+            txtPassword.Clear();
+            txtName.Focus();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //TO-DO: check login user name and password
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Stock;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter(@"SELECT *
+                FROM[Stock].[dbo].[Login] Where UserName='"+ txtName.Text +"' and Password = '"+ txtPassword.Text +"' ", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                this.Hide();
+                StockMain main = new StockMain();
+                main.Show();
+            }
+            else 
+            {
+                MessageBox.Show("Invalid Username or Password..!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                btnClear_Click(sender, e);
+            }
+        }
+    }
+}
---
+++ Stock/Products.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stock
+{
+    public partial class Products : Form
+    {
+        public Products()
+        {
+            InitializeComponent();
+        }
+        
+        private void Products_Load(object sender, EventArgs e)
+        {
+            cmbStatus.SelectedIndex = 0;
+            LoadData();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Stock;Integrated Security=True");
+            //Insert Logic
+            con.Open();
+            bool status = false;
+            if (cmbStatus.SelectedIndex == 0)
+            {
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+
+            var sqlQuery = "";
+
+            if (IfProductsExists(con, txtProductCode.Text))
+            {
+                sqlQuery = @"UPDATE [Products] SET [ProductName] = '" + txtProductName.Text + "' ,[ProductStatus] = '" + status + "' " +
+                            "WHERE [ProductCode] = '" + txtProductCode.Text + "'";
+            }
+            else
+            {
+                sqlQuery = @"INSERT INTO [Stock].[dbo].[Products] ([ProductCode] ,[ProductName] ,[ProductStatus]) VALUES
+                            ('" + txtProductCode.Text + "', '" + txtProductName.Text + "' , '" + status + "')";
+            }
+            SqlCommand cmd = new SqlCommand(sqlQuery,con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            //Reading Data
+            LoadData();         
+        }
+
+        private bool IfProductsExists(SqlConnection con, string productCode)
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("Select 1 From [Products] WHERE [ProductCode] = '" + productCode + "'" , con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count > 0)
+                return true;
+            else
+                return false;
+        }
+        public void LoadData()
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Stock;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter("Select * From [Stock].[dbo].[Products]", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dgvProducts.Rows.Clear();
+            foreach (DataRow item in dt.Rows)
+            {
+                int n = dgvProducts.Rows.Add();
+                dgvProducts.Rows[n].Cells[0].Value = item["ProductCode"].ToString();
+                dgvProducts.Rows[n].Cells[1].Value = item["ProductName"].ToString();
+                if ((bool)item["ProductStatus"])
+                {
+                    dgvProducts.Rows[n].Cells[2].Value = "Active";
+                }
+                else
+                {
+                    dgvProducts.Rows[n].Cells[2].Value = "Deactive";
+                }
+            }
+        }
+
+        private void dgvProducts_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            txtProductCode.Text = dgvProducts.SelectedRows[0].Cells[0].Value.ToString();
+            txtProductName.Text = dgvProducts.SelectedRows[0].Cells[1].Value.ToString();
+            if(dgvProducts.SelectedRows[0].Cells[2].Value.ToString() == "Active")
+            {
+                cmbStatus.SelectedIndex = 0;
+            }
+            else
+            {
+                cmbStatus.SelectedIndex = 1;
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Stock;Integrated Security=True");
+            var sqlQuery = "";
+
+            if (IfProductsExists(con, txtProductCode.Text))
+            {
+                con.Open();
+                sqlQuery = @"DELETE FROM [Products] WHERE [ProductCode] = '" + txtProductCode.Text + "'";
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            else
+            {
+                MessageBox.Show("No record found with that product code");
+            }
+            LoadData();
+        }
+    }
+}
