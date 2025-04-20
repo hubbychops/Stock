d82b33389aae6f030fd3441660bf80b530ccb9c8
hubbychops <47495913+hubbychops@users.noreply.github.com>
2025-04-20 15:06:32 +0100

    PlusForgotPasswordCode
    The Login code has been updated to allow for forgot password option (done with Max W

--- Stock/Login.Designer.cs
+++ Stock/Login.Designer.cs
@@ -36,48 +36,54 @@ namespace Stock
             this.btnClear = new System.Windows.Forms.Button();
             this.btnLogin = new System.Windows.Forms.Button();
             this.lilPassword = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(168, 62);
+            this.txtName.Location = new System.Drawing.Point(112, 40);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(292, 26);
+            this.txtName.Size = new System.Drawing.Size(196, 20);
             this.txtName.TabIndex = 0;
             this.txtName.Text = "admin";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(168, 122);
+            this.txtPassword.Location = new System.Drawing.Point(112, 79);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '#';
-            this.txtPassword.Size = new System.Drawing.Size(292, 26);
+            this.txtPassword.Size = new System.Drawing.Size(196, 20);
             this.txtPassword.TabIndex = 1;
             this.txtPassword.Text = "admin@123";
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(62, 67);
+            this.lblName.Location = new System.Drawing.Point(41, 44);
+            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(97, 20);
+            this.lblName.Size = new System.Drawing.Size(66, 13);
             this.lblName.TabIndex = 2;
             this.lblName.Text = "User Name :";
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(66, 122);
+            this.lblPassword.Location = new System.Drawing.Point(44, 79);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(86, 20);
+            this.lblPassword.Size = new System.Drawing.Size(59, 13);
             this.lblPassword.TabIndex = 3;
             this.lblPassword.Text = "Password :";
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(168, 208);
+            this.btnClear.Location = new System.Drawing.Point(112, 135);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(109, 51);
+            this.btnClear.Size = new System.Drawing.Size(73, 33);
             this.btnClear.TabIndex = 4;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
@@ -85,9 +91,10 @@ namespace Stock
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(353, 208);
+            this.btnLogin.Location = new System.Drawing.Point(235, 135);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(107, 51);
+            this.btnLogin.Size = new System.Drawing.Size(71, 33);
             this.btnLogin.TabIndex = 5;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -96,19 +103,30 @@ namespace Stock
             // lilPassword
             // 
             this.lilPassword.AutoSize = true;
-            this.lilPassword.Location = new System.Drawing.Point(322, 165);
+            this.lilPassword.Location = new System.Drawing.Point(215, 107);
+            this.lilPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lilPassword.Name = "lilPassword";
-            this.lilPassword.Size = new System.Drawing.Size(138, 20);
+            this.lilPassword.Size = new System.Drawing.Size(92, 13);
             this.lilPassword.TabIndex = 6;
             this.lilPassword.TabStop = true;
             this.lilPassword.Text = "Forgot Password?";
+            this.lilPassword.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lilPassword_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
             // 
             // Login
             // 
             this.AcceptButton = this.btnLogin;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(561, 330);
+            this.ClientSize = new System.Drawing.Size(374, 214);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lilPassword);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.btnClear);
@@ -116,7 +134,6 @@ namespace Stock
             this.Controls.Add(this.lblName);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtName);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "                                      Login";
@@ -134,5 +151,6 @@ namespace Stock
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.LinkLabel lilPassword;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Stock/Login.cs
+++ Stock/Login.cs
@@ -4,7 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
+using System.Net.Mail;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -51,5 +55,66 @@ namespace Stock
                 btnClear_Click(sender, e);
             }
         }
+
+        private void lilPassword_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            label1.Visible = false;
+            var username = txtName.Text;
+            //generation of a random 8 digit password through the use of GUID
+            var newPassword = Guid.NewGuid().ToString().Substring(0, 8);
+            if (string.IsNullOrEmpty(username))
+            {
+                label1.Text = "Please enter a user name";
+                label1.Visible = true;
+                return;
+            }
+
+            var con = Connection.GetConnection();
+            var sda = new SqlDataAdapter(@"Select * From [Stock].[dbo].[Login] Where Username = '" + username + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count < 0)
+            {
+                label1.Text = "Invalid user name";
+                return;
+            }
+            var email = dt.Rows[0]["email"].ToString();
+
+            var cmd = new SqlCommand("Update [Stock].[dbo].[Login] Set Password = @Password where username = @username",
+                con);
+            cmd.Parameters.AddWithValue("@Password", newPassword);
+            cmd.Parameters.AddWithValue("@username", username);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            var SMTPClient = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new System.Net.NetworkCredential("dvanoverdijk5@gmail.com", "brwp nvbm froz iony")
+
+            };
+            var mailFrom = new MailAddress("dvanoverdijk5@gmail.com");
+            var mailTo = new MailAddress(email);
+            try
+            {
+                var mailMessage = new MailMessage(mailFrom, mailTo);
+                mailMessage.Subject = "Password Reset";
+                mailMessage.Body = $"Your new password is {newPassword}";
+                SMTPClient.Send(mailMessage);
+                label1.Text = "Password reset email send";
+                label1.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                label1.Text = "An error occurred";
+                label1.Visible = true;
+            }
+        }
+    
     }
 }
